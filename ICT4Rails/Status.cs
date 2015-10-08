f5169f4d1d9b06dd1840bcc31f59f0ca0b121f9e
Yoeri Vink <yoerivi@zeelandnet.nl>
2015-10-08 15:05:03 +0200

    Een paar klasses (niet alles)

---
+++ ICT4Rails/Status.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICT4Rails
+{
+    public class Status
+    {
+        // Fields
+        public string Naam { get; set; }
+
+        public string Bijzonderheden { get; set; }
+
+        public Status(string naam, string bijzonderheden)
+        {
+            Naam = naam;
+            Bijzonderheden = bijzonderheden;
+        }
+    }
+}
---
+++ ICT4Rails/Tram.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICT4Rails
+{
+    public class Tram
+    {
+        public Type Type { get; }
+        public Status Status { get; set; }
+        public int Lijn { get; set; }
+        public int Nummer { get; }
+        public bool Beschikbaar { get; set; }
+        public DateTime Uitrijdtijd { get; set; }
+
+        public Tram (Type type, Status status,int lijn, int nummer, bool beschikbaar, DateTime uitrijdtijd)
+        {
+            Type = type;
+            Status = status;
+            Lijn = lijn;
+            Nummer = nummer;
+            Beschikbaar = beschikbaar;
+            Uitrijdtijd = uitrijdtijd;
+        }
+
+
+        
+    }
+}
