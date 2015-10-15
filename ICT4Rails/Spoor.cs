8949adf7531e365bbb04674c0a4cb15f2b21f7d9
teunvdwijst <teunvdwijst@gmail.com>
2015-10-15 09:30:22 +0200

    aids
    description

---
+++ ICT4Rails/Spoor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICT4Rails
+{
+    public class Spoor
+    {
+        private int spoornummer;
+        private int lijn;
+        private bool beschikbaar;
+
+        public int Spoornummer { get { return spoornummer; } }
+        public int Lijn { get { return lijn; } }
+        public bool Beschikbaar { get { return beschikbaar; } }
+
+        public Spoor(int spoornr, int lijn, bool beschikbaar)
+        {
+            this.spoornummer = spoornr;
+            this.lijn = lijn;
+            this.beschikbaar = beschikbaar;
+        }
+    }
+}
