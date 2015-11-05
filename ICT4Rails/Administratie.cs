12e18418a4907ab096fbf23c4805cff51bd386b6
RobindeKort <Robindekort@hotmail.com>
2015-11-05 13:51:50 +0100

    Merge

--- ICT4Rails/Administratie.cs
+++ ICT4Rails/Administratie.cs
@@ -12,11 +12,13 @@ namespace ICT4Rails
         private List<Gebruiker> gebruikers;
         private List<Medewerker> medewerkers;
         private List<Onderhoud> onderhoudslijst;
+        private List<Spoor> sporen;
         private List<Tram> trams;
 
         public List<Gebruiker> Gebruikers { get { return gebruikers; } }
         public List<Medewerker> Medewerkers { get { return medewerkers; } }
         public List<Onderhoud> Onderhoudslijst { get { return onderhoudslijst; } }
+        public List<Spoor> Sporen { get { return sporen; } }
         public List<Tram> Trams { get { return trams; } }
 
         Database data;
@@ -28,6 +30,7 @@ namespace ICT4Rails
             this.gebruikers.Add(new Gebruiker("", 0, ""));         
             this.medewerkers = data.GetAllMedewerkers();
             this.onderhoudslijst = data.GetAllOnderhoud();
+            this.sporen = data.GetAllSporen();
             this.trams = data.GetAllTrams();
         }
 
