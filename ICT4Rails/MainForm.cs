9eb38564d2de65f55954d558fdfc6258eb17ead1
teunvdwijst <teunvdwijst@gmail.com>
2015-11-09 14:59:23 +0100

    lijst updaten

--- ICT4Rails/MainForm.cs
+++ ICT4Rails/MainForm.cs
@@ -1126,5 +1126,10 @@ namespace ICT4Rails
             administratie.RefreshClass();
             loadListComboboxStatusbeheerOnderhoudMedewerker();
         }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            VulLijsten();
+        }
     }
 }
